6661185b24823698251e4fb4fb22ab546b7a9639
embaabu <edwin.mbaabu@techconsulting.tech>
2024-12-05 20:11:49 -0500

    Adding minimum version to 15.0

--- Sources/NetworkPackage/NetworkMnager.swift
+++ Sources/NetworkPackage/NetworkMnager.swift
@@ -11,7 +11,7 @@ protocol Network{
     //creating a generic function
     func getDataFromUrl<T: Decodable>(url:String ,modelType:T.Type) async throws -> T
 }
-
+@available(macOS 15.0, *)
 class NetworkMnager{
     
     let urlSession : URLSession
